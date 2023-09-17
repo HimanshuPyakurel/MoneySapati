7bde3c394c9cd9e26e6dddf9729d9ea4ab0f8b91
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-09-17 21:47:48 +0545

    Verify User Service

---
+++ src/main/java/global/citytech/moneyexchange/admin/service/VerifyUserService.java
@@ -0,0 +1,7 @@
+package global.citytech.moneyexchange.admin.service;
+
+import global.citytech.moneyexchange.user.model.Users;
+
+public interface VerifyUserService {
+    Users verifyUser(int id);
+}
