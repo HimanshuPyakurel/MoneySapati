08629eba28abb12b879d27776ea37ffd148ff3e4
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-09-18 21:59:34 +0545

    service: UserLoginService

--- src/main/java/global/citytech/moneyexchange/user/service/login/UserLoginService.java
+++ src/main/java/global/citytech/moneyexchange/user/service/login/UserLoginService.java
@@ -1,10 +1,7 @@
 package global.citytech.moneyexchange.user.service.login;
 
 import global.citytech.moneyexchange.user.dto.UsersDTO;
-import global.citytech.moneyexchange.user.model.Users;
 import io.micronaut.http.HttpResponse;
-
-import java.util.Optional;
 
 public interface UserLoginService {
 
