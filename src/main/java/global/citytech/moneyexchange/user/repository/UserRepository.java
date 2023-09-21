98cd6419454bd80972ad0e0a297498a7869ee34a
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-09-21 16:16:12 +0545

    add: User Repository

--- src/main/java/global/citytech/moneyexchange/user/repository/UserRepository.java
+++ src/main/java/global/citytech/moneyexchange/user/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package global.citytech.moneyexchange.user.repository;
 
-import global.citytech.moneyexchange.user.model.UserRoleEnum;
-import global.citytech.moneyexchange.user.model.Users;
+import global.citytech.moneyexchange.constraints.StatusAndRoleEnum;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +13,6 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<Users,Integer> {
     Optional<Users> findByEmailAndPassword(String email,String password);
     Optional<Users> findByEmail(String email);
-    List<Users> findByUserRole(UserRoleEnum userRole);
+    List<Users> findByUserRole(StatusAndRoleEnum userRole);
 
 }
