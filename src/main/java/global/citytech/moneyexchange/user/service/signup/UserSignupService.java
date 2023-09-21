880584e70dc2fc2095adf3a14bf6b0a5d91fcecf
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-09-21 16:18:02 +0545

    feat: UserSignupService added

--- src/main/java/global/citytech/moneyexchange/user/service/signup/UserSignupService.java
+++ src/main/java/global/citytech/moneyexchange/user/service/signup/UserSignupService.java
@@ -1,6 +1,6 @@
 package global.citytech.moneyexchange.user.service.signup;
 
-import global.citytech.moneyexchange.user.model.Users;
+import global.citytech.moneyexchange.user.repository.Users;
 import global.citytech.moneyexchange.user.dto.UsersDTO;
 
 public interface UserSignupService {
