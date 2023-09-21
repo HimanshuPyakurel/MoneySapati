6b76f36ba367d7d6bae281f940a407c4b92d70a5
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-09-21 16:19:09 +0545

    feat: UserGetAllService Added

---
+++ src/main/java/global/citytech/moneyexchange/user/service/getall/UserGetAllService.java
@@ -0,0 +1,9 @@
+package global.citytech.moneyexchange.user.service.getall;
+
+import global.citytech.moneyexchange.user.repository.Users;
+import java.util.List;
+
+public interface UserGetAllService {
+    List<Users> getAllUsers(int id);
+
+}
