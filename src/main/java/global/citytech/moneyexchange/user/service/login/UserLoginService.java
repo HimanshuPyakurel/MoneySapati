4036b86b0727e180c9f8341a3ea42a8243149994
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-09-21 16:16:53 +0545

    feat: Custom Response added in user login service

--- src/main/java/global/citytech/moneyexchange/user/service/login/UserLoginService.java
+++ src/main/java/global/citytech/moneyexchange/user/service/login/UserLoginService.java
@@ -1,10 +1,10 @@
 package global.citytech.moneyexchange.user.service.login;
 
+import global.citytech.moneyexchange.response.CustomResponse;
 import global.citytech.moneyexchange.user.dto.UsersDTO;
-import io.micronaut.http.HttpResponse;
 
 public interface UserLoginService {
 
-      HttpResponse<String> loginUser(UsersDTO usersDTO);
+      CustomResponse loginUser(UsersDTO usersDTO);
 
 }
