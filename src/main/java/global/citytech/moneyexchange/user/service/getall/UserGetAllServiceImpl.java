6b4964f05f2941ea5515d58b4790dac765f4c0df
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-09-21 16:19:26 +0545

    feat: UserGetAllService Implementation Added

---
+++ src/main/java/global/citytech/moneyexchange/user/service/getall/UserGetAllServiceImpl.java
@@ -0,0 +1,46 @@
+package global.citytech.moneyexchange.user.service.getall;
+
+import global.citytech.moneyexchange.constraints.StatusAndRoleEnum;
+import global.citytech.moneyexchange.exception.CustomException;
+import global.citytech.moneyexchange.user.repository.Users;
+import global.citytech.moneyexchange.user.repository.UserRepository;
+import jakarta.inject.Inject;
+import java.util.List;
+import java.util.Optional;
+
+public class UserGetAllServiceImpl implements UserGetAllService {
+
+    private final UserRepository userRepository;
+
+    @Inject
+    public UserGetAllServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<Users> getAllUsers(int id) {
+        Optional<Users> userDto = userRepository.findById(id);
+
+        if (userDto.isPresent()) {
+            Users users = userDto.get();
+
+            if ("Admin".equalsIgnoreCase(users.getUserRole().toString())) {
+                List<Users> allUsers = userRepository.findAll();
+                if (allUsers.isEmpty()) {
+                    throw new CustomException("No users found");
+                }
+                return allUsers;
+            }
+            else if(users.getCheckStatus().equals("Pending")){
+                throw new CustomException("User Status not Verified");
+            }
+            else if ("Lender".equalsIgnoreCase(users.getUserRole().toString())) {
+                return userRepository.findByUserRole(StatusAndRoleEnum.BORROWER);
+            } else {
+                return userRepository.findByUserRole(StatusAndRoleEnum.LENDER);
+            }
+        }
+        throw new CustomException("User not found");
+    }
+
+}
