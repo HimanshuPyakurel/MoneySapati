fee72c73673ee736ac36496cd1d756e33598bdee
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-09-24 22:37:59 +0545

    feat: user get all request

---
+++ src/main/java/global/citytech/moneyexchange/user/service/getall/UserGetAllRequest.java
@@ -0,0 +1,23 @@
+package global.citytech.moneyexchange.user.service.getall;
+
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.serde.annotation.Serdeable;
+
+@Introspected
+@Serdeable
+public class UserGetAllRequest {
+
+    private int userId;
+
+    public UserGetAllRequest(int userId) {
+        this.userId = userId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
