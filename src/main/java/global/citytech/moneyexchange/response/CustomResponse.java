25f5515863cff63437fd5d071939b44a5417013b
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-09-25 19:38:50 +0545

    Custom response created

--- src/main/java/global/citytech/moneyexchange/response/CustomResponse.java
+++ src/main/java/global/citytech/moneyexchange/response/CustomResponse.java
@@ -5,26 +5,26 @@ import io.micronaut.serde.annotation.Serdeable;
 @Serdeable
 public class CustomResponse {
     private final String message;
-    private final boolean successStatus;
+    private final boolean isSuccess;
 
-    public CustomResponse(String message, boolean successStatus) {
+    public CustomResponse(String message,boolean isSuccess) {
         this.message = message;
-        this.successStatus = successStatus;
+        this.isSuccess=isSuccess;
     }
 
     public String getMessage() {
         return message;
     }
 
-    public boolean isSuccessStatus() {
-        return successStatus;
+    public boolean isSuccess() {
+        return isSuccess;
     }
 
     @Override
     public String toString() {
         return "CustomResponse{" +
                 "message='" + message + '\'' +
-                ", successStatus=" + successStatus +
+                ", isSuccess=" + isSuccess +
                 '}';
     }
 }
