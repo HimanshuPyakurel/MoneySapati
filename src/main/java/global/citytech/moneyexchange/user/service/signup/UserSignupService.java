f282f005995eee6b8fcf568b3af4eb7b74af7abd
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-09-25 19:37:34 +0545

    feat: Custom response added

--- src/main/java/global/citytech/moneyexchange/user/service/signup/UserSignupService.java
+++ src/main/java/global/citytech/moneyexchange/user/service/signup/UserSignupService.java
@@ -1,9 +1,10 @@
 package global.citytech.moneyexchange.user.service.signup;
 
+import global.citytech.moneyexchange.response.CustomResponse;
 import global.citytech.moneyexchange.user.repository.Users;
 import global.citytech.moneyexchange.user.dto.UsersDTO;
 
 public interface UserSignupService {
 
-    Users signup(UsersDTO usersDto);
+    CustomResponse signup(UsersDTO usersDto);
 }
