f49f56cee305ea70f739c6f40e5a5a83c43e6dda
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-02 13:56:13 +0545

    borrower return money either partially or completely

--- src/main/java/global/citytech/moneyexchange/transaction/service/borrower/returntransactionamount/ReturnTransactionAmountServiceImpl.java
+++ src/main/java/global/citytech/moneyexchange/transaction/service/borrower/returntransactionamount/ReturnTransactionAmountServiceImpl.java
@@ -11,6 +11,7 @@ import global.citytech.moneyexchange.user.repository.UserRepository;
 import global.citytech.moneyexchange.user.repository.Users;
 import jakarta.inject.Inject;
 
+import java.util.List;
 import java.util.Optional;
 
 public class ReturnTransactionAmountServiceImpl implements ReturnTransactionAmountService {
@@ -30,56 +31,74 @@ public class ReturnTransactionAmountServiceImpl implements ReturnTransactionAmou
     @Override
     public CustomResponse returnTransaction(ReturnTransactionAmountRequest request) {
         Optional<CashDetails> cashDetails = cashDetailsRepository.findById(request.getCashId());
-        Optional<Transaction> transactionDetails = transactionRepository.findByBorrowerId(cashDetails.get().getBorrowerId());
+        List<Transaction> transactionDetails = transactionRepository.findByBorrowerId(cashDetails.get().getBorrowerId());
+        Optional<Users> lender = userRepository.findById(cashDetails.get().getLenderId());
+        Optional<Users> borrower =userRepository.findById(cashDetails.get().getBorrowerId());
 
-        if(cashDetails.isPresent()){
-            if(transactionDetails.isPresent()){
-
-                double lenderAmount = cashDetails.get().getLenderAmount();
-                double borrowerAmount = cashDetails.get().getBorrowerAmount();
-                CashDetails updateCashDetails = new CashDetails();
-                Transaction updateTransaction = transactionDetails.get();
-
-                this.validateRequest(request);
-
-                updateCashDetails.setId(request.getCashId());
-                updateCashDetails.setBorrowerId(request.getBorrowerId());
-                updateCashDetails.setLenderId(request.getLenderId());
-                updateCashDetails.setBorrowerAmount(borrowerAmount - request.getReturningAmount());
-                updateCashDetails.setLenderAmount(lenderAmount + request.getReturningAmount());
-
-                if (borrowerAmount == request.getReturningAmount()) {
-                    updateCashDetails.setStatus(StatusAndRoleEnum.COMPLETE.name());
-                    updateTransaction.setTransactionStatus(StatusAndRoleEnum.COMPLETE.name());
-                    transactionRepository.update(updateTransaction);
-                }
-
-                if (borrowerAmount > request.getReturningAmount()) {
-                    updateCashDetails.setStatus(StatusAndRoleEnum.PARTIAL_PAID.name());
-                    updateTransaction.setTransactionStatus(StatusAndRoleEnum.PARTIAL_PAID.name());
-                    transactionRepository.update(updateTransaction);
-                }
-
-                if (borrowerAmount < request.getReturningAmount()) {
-                    throw new CustomException("Returning Amount is more than actual initiated money");
-                }
-
-                cashDetailsRepository.update(updateCashDetails);
-                return new CustomResponse("Successfully completed payment",true);
-
-            }
-            else throw new CustomException("Borrower id or lender id not matched");
+        if (cashDetails.isEmpty()) {
+            throw new CustomException("Cash detail id not found");
         }
-        else throw new CustomException("Cash detail id not found");
-    }
+        if (transactionDetails.isEmpty()) {
+            throw new CustomException("Borrower id or lender id not matched");
+        }
+        if(lender.isEmpty()){
+            throw new CustomException("User not found");
+        }
+        if(borrower.isEmpty()){
+            throw new CustomException("User not found");
+        }
 
+        double lenderAmount = cashDetails.get().getTotalLenderAvailableAmount();
+        double borrowerAmount = cashDetails.get().getBorrowerAmountToPay();
+        CashDetails updateCashDetails = new CashDetails();
+        Transaction updateTransaction = transactionDetails.get(0);
+
+        this.validateRequest(request);
+
+        updateCashDetails.setId(request.getCashId());
+        updateCashDetails.setBorrowerId(request.getBorrowerId());
+        updateCashDetails.setLenderId(request.getLenderId());
+        updateCashDetails.setBorrowerAmountToPay(borrowerAmount - request.getReturningAmount());
+        updateCashDetails.setTotalLenderAvailableAmount(lenderAmount + request.getReturningAmount());
+
+        if (borrower.isPresent()) {
+            Users userUpdate = borrower.get();
+            userUpdate.setAvailableBalance(borrowerAmount-request.getReturningAmount());
+            userRepository.update(userUpdate);
+        }
+        if(lender.isPresent()){
+            Users userUpdate = lender.get();
+            userUpdate.setAvailableBalance(userUpdate.getAvailableBalance() + request.getReturningAmount());
+            userRepository.update(userUpdate);
+        }
+
+        if (borrowerAmount == request.getReturningAmount()) {
+            updateCashDetails.setStatus(StatusAndRoleEnum.COMPLETE.name());
+            updateTransaction.setTransactionStatus(StatusAndRoleEnum.COMPLETE.name());
+            transactionRepository.update(updateTransaction);
+        }
+
+        if (borrowerAmount > request.getReturningAmount()) {
+            updateCashDetails.setStatus(StatusAndRoleEnum.PARTIAL_PAID.name());
+            updateTransaction.setTransactionStatus(StatusAndRoleEnum.PARTIAL_PAID.name());
+            transactionRepository.update(updateTransaction);
+        }
+
+        if (borrowerAmount < request.getReturningAmount()) {
+            throw new CustomException("Returning Amount is more than actual initiated money");
+        }
+
+        cashDetailsRepository.update(updateCashDetails);
+        return new CustomResponse("Successfully completed payment", true);
+
+    }
 
     public void validateRequest(ReturnTransactionAmountRequest request) {
         Optional<CashDetails> cashDetails = cashDetailsRepository.findById(request.getCashId());
-        Optional<Transaction> lenderTransaction = transactionRepository.findByLenderId(request.getLenderId());
-        Optional<Transaction> borrowerTransaction = transactionRepository.findByBorrowerId(request.getBorrowerId());
-        Optional<CashDetails> lenderCashDetails = cashDetailsRepository.findByLenderId(request.getLenderId());
-        Optional<CashDetails> borrowerCashDetails = cashDetailsRepository.findByBorrowerId(request.getBorrowerId());
+        List<Transaction> lenderTransaction = transactionRepository.findByLenderId(request.getLenderId());
+        List<Transaction> borrowerTransaction = transactionRepository.findByBorrowerId(request.getBorrowerId());
+        List<CashDetails> lenderCashDetails = cashDetailsRepository.findByLenderId(request.getLenderId());
+        List<CashDetails> borrowerCashDetails = cashDetailsRepository.findByBorrowerId(request.getBorrowerId());
 
         if (cashDetails.isEmpty()) {
             throw new CustomException("Cash Details Not Found");
@@ -90,7 +109,7 @@ public class ReturnTransactionAmountServiceImpl implements ReturnTransactionAmou
         if (lenderTransaction.isEmpty() && lenderCashDetails.isEmpty()) {
             throw new CustomException("Lender Not Found. Enter lender id properly");
         }
-        if (cashDetails.get().getBorrowerAmount()<= 0){
+        if (cashDetails.get().getBorrowerAmountToPay() <= 0) {
             throw new CustomException("No amount left to be paid");
         }
 
