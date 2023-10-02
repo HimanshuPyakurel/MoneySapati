aa47879302c58865fc2533605a40946a777e95f8
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-02 13:56:36 +0545

    repository: transaction

--- src/main/java/global/citytech/moneyexchange/transaction/repository/transaction/TransactionRepository.java
+++ src/main/java/global/citytech/moneyexchange/transaction/repository/transaction/TransactionRepository.java
@@ -1,15 +1,18 @@
 package global.citytech.moneyexchange.transaction.repository.transaction;
 
+import global.citytech.moneyexchange.constraints.StatusAndRoleEnum;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 @JdbcRepository(dialect = Dialect.POSTGRES)
 public interface TransactionRepository extends CrudRepository<Transaction,Integer> {
-    Optional<Transaction> findByBorrowerId(int borrowerId);
-    Optional<Transaction> findByLenderId(int lenderId);
+    List<Transaction> findByBorrowerId(int borrowerId);
+    List<Transaction> findByLenderId(int lenderId);
+    List<Transaction> findByLenderIdAndTransactionStatus(int lenderId, String transactionStatus);
 }
