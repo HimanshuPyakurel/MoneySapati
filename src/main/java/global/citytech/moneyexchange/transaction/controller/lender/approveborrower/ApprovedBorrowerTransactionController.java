73db490cc0974cf55bfbf84bd92d396b57bcd078
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-03 20:53:07 +0545

    lender approve borrower request controller

---
+++ src/main/java/global/citytech/moneyexchange/transaction/controller/lender/approveborrower/ApprovedBorrowerTransactionController.java
@@ -0,0 +1,28 @@
+package global.citytech.moneyexchange.transaction.controller.lender.approveborrower;
+
+import global.citytech.moneyexchange.response.CustomResponse;
+import global.citytech.moneyexchange.transaction.service.lender.approveborrower.ApproveBorrowerService;
+import global.citytech.moneyexchange.transaction.service.lender.approveborrower.ApprovedTransactionRequest;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import jakarta.inject.Inject;
+
+@Controller("/lender")
+public class ApprovedBorrowerTransactionController {
+    public final ApproveBorrowerService approveBorrowerService;
+
+    @Inject
+    public ApprovedBorrowerTransactionController(ApproveBorrowerService approveBorrowerService){
+        this.approveBorrowerService = approveBorrowerService;
+    }
+
+    @Post("/verify")
+    public CustomResponse approveBorrowerRequest(@Body ApprovedTransactionRequest request){
+        approveBorrowerService.approvedTransaction(request);
+        return new CustomResponse("Successfully approved borrower requested money",true);
+    }
+
+
+
+}
