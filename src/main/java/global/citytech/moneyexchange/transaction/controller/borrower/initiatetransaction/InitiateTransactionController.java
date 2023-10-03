83382eb239587d8c657258c1ca655dcdf295b140
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-03 20:52:22 +0545

    borrower initiate transaction controller

---
+++ src/main/java/global/citytech/moneyexchange/transaction/controller/borrower/initiatetransaction/InitiateTransactionController.java
@@ -0,0 +1,27 @@
+package global.citytech.moneyexchange.transaction.controller.borrower.initiatetransaction;
+
+import global.citytech.moneyexchange.response.CustomResponse;
+import global.citytech.moneyexchange.transaction.service.borrower.initiatetransactionrequest.InitiateTransactionRequest;
+import global.citytech.moneyexchange.transaction.service.borrower.initiatetransactionrequest.InitiateTransactionService;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import jakarta.inject.Inject;
+
+@Controller("/transaction")
+public class InitiateTransactionController {
+
+    public final InitiateTransactionService initiateTransactionService;
+
+    @Inject
+    public InitiateTransactionController(InitiateTransactionService initiateTransactionService){
+        this.initiateTransactionService=initiateTransactionService;
+    }
+
+    @Post("/request")
+    public CustomResponse borrowerRequest(@Body InitiateTransactionRequest request){
+        initiateTransactionService.initiateTransaction(request);
+        return new CustomResponse("Successfully requested money",true);
+    }
+
+}
