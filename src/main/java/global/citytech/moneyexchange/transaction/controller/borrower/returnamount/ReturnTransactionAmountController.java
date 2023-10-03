3c01ee76a09586c7c00412d52cd2dc4e8b0ec17b
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-03 20:52:42 +0545

    borrower return money to lender controller

---
+++ src/main/java/global/citytech/moneyexchange/transaction/controller/borrower/returnamount/ReturnTransactionAmountController.java
@@ -0,0 +1,26 @@
+package global.citytech.moneyexchange.transaction.controller.borrower.returnamount;
+
+import global.citytech.moneyexchange.response.CustomResponse;
+import global.citytech.moneyexchange.transaction.service.borrower.returntransactionamount.ReturnTransactionAmountRequest;
+import global.citytech.moneyexchange.transaction.service.borrower.returntransactionamount.ReturnTransactionAmountService;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import jakarta.inject.Inject;
+
+@Controller("/borrower")
+public class ReturnTransactionAmountController {
+    private ReturnTransactionAmountService returnTransactionAmountService;
+
+    @Inject
+    public ReturnTransactionAmountController(ReturnTransactionAmountService returnTransactionAmountService){
+        this.returnTransactionAmountService=returnTransactionAmountService;
+    }
+
+    @Post("/return")
+    public CustomResponse returnAmount(@Body ReturnTransactionAmountRequest returnTransactionAmountRequest){
+        returnTransactionAmountService.returnTransaction(returnTransactionAmountRequest);
+        return new CustomResponse("Successfully returned money",true);
+    }
+
+}
