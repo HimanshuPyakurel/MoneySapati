ec43b7d2c7c7e0e5bcd7133d20aed93bde97183b
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-05 22:46:41 +0545

    service: Admin verify user status

--- src/main/java/global/citytech/moneyexchange/admin/service/verifyuser/VerifyUserService.java
+++ src/main/java/global/citytech/moneyexchange/admin/service/verifyuser/VerifyUserService.java
@@ -1,6 +1,6 @@
 package global.citytech.moneyexchange.admin.service.verifyuser;
 
-import global.citytech.moneyexchange.response.CustomResponse;
+import global.citytech.moneyexchange.platform.response.CustomResponse;
 
 public interface VerifyUserService {
     CustomResponse verifyUser(VerifyUserRequest verifyUserRequest);
