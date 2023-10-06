16a9ea0b8f2ebbfe87b6977d1f13480d89519311
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-06 08:11:10 +0545

    Enum: Status and Role enum

---
+++ src/main/java/global/citytech/moneyexchange/platform/constraints/StatusAndRoleEnum.java
@@ -0,0 +1,5 @@
+package global.citytech.moneyexchange.platform.constraints;
+
+public enum StatusAndRoleEnum {
+    BORROWER, LENDER, ADMIN,PENDING,VERIFIED,UNPAID,PARTIAL_PAID, COMPLETE,REJECTED, BLACKLIST
+}
