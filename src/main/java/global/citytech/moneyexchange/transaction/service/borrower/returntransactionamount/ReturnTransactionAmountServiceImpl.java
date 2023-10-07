5fd0e8e08e30bde32633141d2a58fa23472143dc
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-07 15:23:16 +0545

    borrower return money to lender service impl

--- src/main/java/global/citytech/moneyexchange/transaction/service/borrower/returntransactionamount/ReturnTransactionAmountServiceImpl.java
+++ src/main/java/global/citytech/moneyexchange/transaction/service/borrower/returntransactionamount/ReturnTransactionAmountServiceImpl.java
@@ -1,8 +1,8 @@
 package global.citytech.moneyexchange.transaction.service.borrower.returntransactionamount;
 
-import global.citytech.moneyexchange.constraints.StatusAndRoleEnum;
-import global.citytech.moneyexchange.exception.CustomException;
-import global.citytech.moneyexchange.response.CustomResponse;
+import global.citytech.moneyexchange.platform.constraints.StatusAndRoleEnum;
+import global.citytech.moneyexchange.platform.exception.CustomException;
+import global.citytech.moneyexchange.platform.response.CustomResponse;
 import global.citytech.moneyexchange.transaction.repository.cashdetails.CashDetails;
 import global.citytech.moneyexchange.transaction.repository.cashdetails.CashDetailsRepository;
 import global.citytech.moneyexchange.transaction.repository.transaction.Transaction;
@@ -35,42 +35,29 @@ public class ReturnTransactionAmountServiceImpl implements ReturnTransactionAmou
         Optional<Users> lender = userRepository.findById(cashDetails.get().getLenderId());
         Optional<Users> borrower =userRepository.findById(cashDetails.get().getBorrowerId());
 
-        if (cashDetails.isEmpty()) {
-            throw new CustomException("Cash detail id not found");
-        }
-        if (transactionDetails.isEmpty()) {
-            throw new CustomException("Borrower id or lender id not matched");
-        }
-        if(lender.isEmpty()){
-            throw new CustomException("User not found");
-        }
-        if(borrower.isEmpty()){
-            throw new CustomException("User not found");
-        }
-
-        double lenderAmount = cashDetails.get().getTotalLenderAvailableAmount();
         double borrowerAmount = cashDetails.get().getBorrowerAmountToPay();
         CashDetails updateCashDetails = new CashDetails();
         Transaction updateTransaction = transactionDetails.get(0);
 
         this.validateRequest(request);
 
-        updateCashDetails.setId(request.getCashId());
-        updateCashDetails.setBorrowerId(request.getBorrowerId());
-        updateCashDetails.setLenderId(request.getLenderId());
-        updateCashDetails.setBorrowerAmountToPay(borrowerAmount - request.getReturningAmount());
-        updateCashDetails.setTotalLenderAvailableAmount(lenderAmount + request.getReturningAmount());
-
         if (borrower.isPresent()) {
             Users userUpdate = borrower.get();
             userUpdate.setAvailableBalance(borrowerAmount-request.getReturningAmount());
             userRepository.update(userUpdate);
         }
+
         if(lender.isPresent()){
             Users userUpdate = lender.get();
             userUpdate.setAvailableBalance(userUpdate.getAvailableBalance() + request.getReturningAmount());
             userRepository.update(userUpdate);
         }
+
+        updateCashDetails.setId(request.getCashId());
+        updateCashDetails.setBorrowerId(request.getBorrowerId());
+        updateCashDetails.setLenderId(request.getLenderId());
+        updateCashDetails.setBorrowerAmountToPay(borrowerAmount - request.getReturningAmount());
+        updateCashDetails.setTotalLenderAvailableAmount(lender.get().getAvailableBalance());
 
         if (borrowerAmount == request.getReturningAmount()) {
             updateCashDetails.setStatus(StatusAndRoleEnum.COMPLETE.name());
@@ -80,12 +67,16 @@ public class ReturnTransactionAmountServiceImpl implements ReturnTransactionAmou
 
         if (borrowerAmount > request.getReturningAmount()) {
             updateCashDetails.setStatus(StatusAndRoleEnum.PARTIAL_PAID.name());
-            updateTransaction.setTransactionStatus(StatusAndRoleEnum.PARTIAL_PAID.name());
+            updateTransaction.setTransactionStatus(StatusAndRoleEnum.UNPAID.name());
             transactionRepository.update(updateTransaction);
         }
 
         if (borrowerAmount < request.getReturningAmount()) {
-            throw new CustomException("Returning Amount is more than actual initiated money");
+            throw new CustomException(400,"Returning Amount is more than actual initiated money");
+        }
+
+        if (borrowerAmount <= 0) {
+            throw new CustomException(400,"No amount left to be paid");
         }
 
         cashDetailsRepository.update(updateCashDetails);
@@ -93,7 +84,7 @@ public class ReturnTransactionAmountServiceImpl implements ReturnTransactionAmou
 
     }
 
-    public void validateRequest(ReturnTransactionAmountRequest request) {
+    private void validateRequest(ReturnTransactionAmountRequest request) {
         Optional<CashDetails> cashDetails = cashDetailsRepository.findById(request.getCashId());
         List<Transaction> lenderTransaction = transactionRepository.findByLenderId(request.getLenderId());
         List<Transaction> borrowerTransaction = transactionRepository.findByBorrowerId(request.getBorrowerId());
@@ -101,18 +92,25 @@ public class ReturnTransactionAmountServiceImpl implements ReturnTransactionAmou
         List<CashDetails> borrowerCashDetails = cashDetailsRepository.findByBorrowerId(request.getBorrowerId());
 
         if (cashDetails.isEmpty()) {
-            throw new CustomException("Cash Details Not Found");
+            throw new CustomException(400,"Cash Details Not Found");
         }
         if (borrowerTransaction.isEmpty() && borrowerCashDetails.isEmpty()) {
-            throw new CustomException("Borrower not found. Enter borrower id properly");
+            throw new CustomException(400,"Borrower not found. Enter borrower id properly");
         }
         if (lenderTransaction.isEmpty() && lenderCashDetails.isEmpty()) {
-            throw new CustomException("Lender Not Found. Enter lender id properly");
+            throw new CustomException(400,"Lender Not Found. Enter lender id properly");
         }
         if (cashDetails.get().getBorrowerAmountToPay() <= 0) {
-            throw new CustomException("No amount left to be paid");
+            throw new CustomException(400,"No amount left to be paid");
         }
 
+        if(lenderCashDetails.isEmpty()){
+            throw new CustomException(400,"User not found");
+        }
+
+        if(borrowerCashDetails.isEmpty()){
+            throw new CustomException(400,"User not found");
+        }
     }
 
 }
