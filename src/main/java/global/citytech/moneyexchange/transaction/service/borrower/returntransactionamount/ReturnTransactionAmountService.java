e2210b392c9078da1f9a72f574bd065aa3e92cd9
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-07 15:23:04 +0545

    borrower return money to lender service

--- src/main/java/global/citytech/moneyexchange/transaction/service/borrower/returntransactionamount/ReturnTransactionAmountService.java
+++ src/main/java/global/citytech/moneyexchange/transaction/service/borrower/returntransactionamount/ReturnTransactionAmountService.java
@@ -1,6 +1,6 @@
 package global.citytech.moneyexchange.transaction.service.borrower.returntransactionamount;
 
-import global.citytech.moneyexchange.response.CustomResponse;
+import global.citytech.moneyexchange.platform.response.CustomResponse;
 
 public interface ReturnTransactionAmountService {
     CustomResponse returnTransaction(ReturnTransactionAmountRequest returnTransactionRequest);
