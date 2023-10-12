9140fa04834edb9daf53a83cfcd3c6ebf50775cb
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-12 13:12:02 +0545

    admin reject user service implementation

--- src/main/java/global/citytech/moneyexchange/admin/service/rejectuser/RejectUserServiceImpl.java
+++ src/main/java/global/citytech/moneyexchange/admin/service/rejectuser/RejectUserServiceImpl.java
@@ -2,12 +2,10 @@ package global.citytech.moneyexchange.admin.service.rejectuser;
 
 import global.citytech.moneyexchange.platform.constraints.StatusAndRoleEnum;
 import global.citytech.moneyexchange.platform.exception.CustomException;
-import global.citytech.moneyexchange.platform.response.CustomResponse;
+import global.citytech.moneyexchange.platform.security.ContextHolder;
 import global.citytech.moneyexchange.user.repository.UserRepository;
 import global.citytech.moneyexchange.user.repository.Users;
 import jakarta.inject.Inject;
-import org.springframework.util.DigestUtils;
-
 import java.util.Optional;
 
 public class RejectUserServiceImpl implements RejectUserService{
@@ -19,15 +17,20 @@ public class RejectUserServiceImpl implements RejectUserService{
     }
 
     @Override
-    public CustomResponse rejectUser(RejectUserRequest rejectUserRequest) {
+    public Optional<RejectUserResponse> rejectUser(RejectUserRequest rejectUserRequest) {
 
-        Optional<Users> userList = userRepository.findByEmail(rejectUserRequest.getAdminEmail());
-        String checkPassword = DigestUtils.md5DigestAsHex(rejectUserRequest.getAdminPassword().getBytes());
+        System.out.println("......" + ContextHolder.get());
+        Optional<Users> userList = userRepository.findById(rejectUserRequest.getUserId());
 
-        if (userList.isPresent() && userList.get().getPassword().equals(checkPassword)) {
-            if (userList.get().getUserRole() == StatusAndRoleEnum.ADMIN) {
-                this.validateRequest(rejectUserRequest.getUserId());
-                return new CustomResponse("User rejected",true);
+        if (userList.isPresent()) {
+            if ("ADMIN".equalsIgnoreCase(ContextHolder.get().userRole())) {
+
+                this.rejectUser(rejectUserRequest.getUserId());
+
+                return Optional.of(new RejectUserResponse(userList.get().getFirstName(),userList.get().getLastName(),
+                        userList.get().getUserName(),userList.get().getEmail(),userList.get().getCitizenship(),
+                        userList.get().getUserRole().name(),StatusAndRoleEnum.REJECTED.name(),
+                        StatusAndRoleEnum.REJECTED.name(),"User Rejected successfully"));
             } else {
                 throw new CustomException(400,"Only Admin User can verify the user details");
             }
@@ -35,13 +38,12 @@ public class RejectUserServiceImpl implements RejectUserService{
         throw new CustomException(400,"Invalid Admin UserName or Password");
     }
 
-    private CustomResponse validateRequest(int id){
+    private Users rejectUser(int id){
         Optional<Users> user = userRepository.findById(id);
         if(user.isPresent()){
-            user.get().setCheckStatus("Rejected");
-            user.get().setCheckBlacklist("Rejected");
-            userRepository.update(user.get());
-            return new CustomResponse("User Successfully Validated",true);
+            user.get().setCheckStatus(StatusAndRoleEnum.REJECTED.name());
+            user.get().setCheckBlacklist(StatusAndRoleEnum.REJECTED.name());
+            return userRepository.update(user.get());
         }
         throw  new CustomException(400,"User not found");
     }
