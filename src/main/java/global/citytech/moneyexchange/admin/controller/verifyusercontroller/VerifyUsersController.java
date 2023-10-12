083b119844d071a72fbc536b35ebb08c91688e3f
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-12 13:10:58 +0545

    admin verify user controller

--- src/main/java/global/citytech/moneyexchange/admin/controller/verifyusercontroller/VerifyUsersController.java
+++ src/main/java/global/citytech/moneyexchange/admin/controller/verifyusercontroller/VerifyUsersController.java
@@ -2,8 +2,11 @@ package global.citytech.moneyexchange.admin.controller.verifyusercontroller;
 
 import global.citytech.moneyexchange.admin.service.verifyuser.VerifyUserRequest;
 import global.citytech.moneyexchange.admin.service.verifyuser.VerifyUserService;
-import global.citytech.moneyexchange.platform.response.CustomResponse;
-import io.micronaut.http.annotation.*;
+import global.citytech.moneyexchange.platform.response.RestResponse;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
 import jakarta.inject.Inject;
 
 @Controller("/user")
@@ -16,9 +19,9 @@ public class VerifyUsersController {
     }
 
     @Post("/verify")
-    public CustomResponse verifyUser(@Body VerifyUserRequest verifyUserRequest){
-        verifyUserService.verifyUser(verifyUserRequest);
-        return new CustomResponse("Verified User Successfully",true);
+    public HttpResponse<RestResponse> verifyUser(@Body VerifyUserRequest verifyUserRequest) {
+        var response = verifyUserService.verifyUser(verifyUserRequest);
+        return HttpResponse.ok(RestResponse.success(response.get()));
     }
 
 
