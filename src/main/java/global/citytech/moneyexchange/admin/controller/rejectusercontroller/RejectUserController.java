b2f05d2b1283f60da828ed810134896543ccc709
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-10-12 13:10:39 +0545

    admin reject user controller

--- src/main/java/global/citytech/moneyexchange/admin/controller/rejectusercontroller/RejectUserController.java
+++ src/main/java/global/citytech/moneyexchange/admin/controller/rejectusercontroller/RejectUserController.java
@@ -2,7 +2,10 @@ package global.citytech.moneyexchange.admin.controller.rejectusercontroller;
 
 import global.citytech.moneyexchange.admin.service.rejectuser.RejectUserRequest;
 import global.citytech.moneyexchange.admin.service.rejectuser.RejectUserService;
+import global.citytech.moneyexchange.admin.service.verifyuser.VerifyUserRequest;
 import global.citytech.moneyexchange.platform.response.CustomResponse;
+import global.citytech.moneyexchange.platform.response.RestResponse;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
@@ -18,9 +21,10 @@ public class RejectUserController {
     }
 
     @Post("/reject")
-    public CustomResponse rejectUser(@Body RejectUserRequest rejectUserRequest){
-        rejectUserService.rejectUser(rejectUserRequest);
-        return new CustomResponse("Rejected User Successfully",true);
+    public HttpResponse<RestResponse> rejectUser(@Body RejectUserRequest rejectUserRequest){
+        var response = rejectUserService.rejectUser(rejectUserRequest);
+        return HttpResponse.ok(RestResponse.success(response.get()));
     }
+
 
 }
