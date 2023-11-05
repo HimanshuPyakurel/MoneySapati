ed90679addb2fa8e1cbd825f1ba704ce13192c24
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-11-05 17:07:33 +0545

    admin verify user request implementation

--- src/main/java/global/citytech/moneyexchange/admin/service/verifyuser/VerifyUserServiceImpl.java
+++ src/main/java/global/citytech/moneyexchange/admin/service/verifyuser/VerifyUserServiceImpl.java
@@ -3,6 +3,7 @@ package global.citytech.moneyexchange.admin.service.verifyuser;
 import global.citytech.moneyexchange.platform.constraints.StatusAndRoleEnum;
 import global.citytech.moneyexchange.platform.exception.CustomException;
 import global.citytech.moneyexchange.platform.response.CustomResponse;
+import global.citytech.moneyexchange.platform.security.ContextHolder;
 import global.citytech.moneyexchange.user.repository.Users;
 import global.citytech.moneyexchange.user.repository.UserRepository;
 import jakarta.inject.Inject;
@@ -19,15 +20,18 @@ public class VerifyUserServiceImpl implements VerifyUserService {
     }
 
     @Override
-    public CustomResponse verifyUser(VerifyUserRequest verifyUserRequest) {
+    public Optional<VerifyUserResponse> verifyUser(VerifyUserRequest verifyUserRequest) {
 
-        Optional<Users> userList=  userRepository.findByEmail(verifyUserRequest.getAdminEmail());
-        String checkPassword = DigestUtils.md5DigestAsHex(verifyUserRequest.getAdminPassword().getBytes());
+        System.out.println("......" + ContextHolder.get());
+        Optional<Users> userList =  userRepository.findById(verifyUserRequest.getUserId());
 
-        if(userList.isPresent() && userList.get().getPassword().equals(checkPassword)) {
-            if ("ADMIN".equalsIgnoreCase(userList.get().getUserRole().name())) {
+        if(userList.isPresent() ) {
+            if ("ADMIN".equalsIgnoreCase(ContextHolder.get().userRole())) {
                 this.validateRequest(verifyUserRequest.getUserId());
-                return new CustomResponse("User status update successfully",true);
+                return Optional.of(new VerifyUserResponse(userList.get().getFirstName(),userList.get().getLastName(),
+                        userList.get().getUserName(),userList.get().getEmail(),userList.get().getCitizenship(),
+                        userList.get().getUserRole().name(),StatusAndRoleEnum.VERIFIED.name(),
+                        StatusAndRoleEnum.VERIFIED.name(),"User Verified successfully"));
             } else {
                 throw new CustomException(400,"Only Admin User can verify the user details");
             }
@@ -35,13 +39,12 @@ public class VerifyUserServiceImpl implements VerifyUserService {
         throw new CustomException(400,"Invalid Admin UserName or Password");
     }
 
-    private CustomResponse validateRequest(int id){
+    private Users validateRequest(int id){
         Optional<Users> user = userRepository.findById(id);
         if(user.isPresent()){
             user.get().setCheckStatus(StatusAndRoleEnum.VERIFIED.name());
             user.get().setCheckBlacklist(StatusAndRoleEnum.VERIFIED.name());
-            userRepository.update(user.get());
-            return new CustomResponse("User Successfully Validated",true);
+            return userRepository.update(user.get());
         }
         throw  new CustomException(400,"User not found");
     }
