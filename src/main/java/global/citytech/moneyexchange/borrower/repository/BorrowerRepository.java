5d0e3793400b0c025de4723aed1aa5d863a20159
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-11-05 17:09:52 +0545

    repository: borrowerRepository

--- src/main/java/global/citytech/moneyexchange/borrower/repository/BorrowerRepository.java
+++
@@ -1,7 +0,0 @@
-package global.citytech.moneyexchange.borrower.repository;
-
-import global.citytech.moneyexchange.borrower.model.Borrower;
-import io.micronaut.data.repository.CrudRepository;
-
-public interface BorrowerRepository extends CrudRepository<Borrower,Integer> {
-}
