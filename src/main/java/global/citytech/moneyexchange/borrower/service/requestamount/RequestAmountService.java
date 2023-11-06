3323e52afd6ee5fe2189c765259d2fbd611b5799
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-11-06 08:45:56 +0545

    borrower request money service

--- src/main/java/global/citytech/moneyexchange/borrower/service/requestamount/RequestAmountService.java
+++
@@ -1,9 +0,0 @@
-package global.citytech.moneyexchange.borrower.service.requestamount;
-
-import global.citytech.moneyexchange.borrower.dto.BorrowerDto;
-import global.citytech.moneyexchange.borrower.model.Borrower;
-import io.micronaut.http.HttpResponse;
-
-public interface RequestAmountService {
-    Borrower createTransaction(BorrowerDto borrowerDto);
-}
