56dfbedbe21d099b881fa45b9bfaa2f95757df71
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-11-14 09:13:00 +0545

    User login controller

--- src/main/java/global/citytech/moneyexchange/user/controller/login/UserLoginController.java
+++ src/main/java/global/citytech/moneyexchange/user/controller/login/UserLoginController.java
@@ -1,12 +1,17 @@
 package global.citytech.moneyexchange.user.controller.login;
 
-import global.citytech.moneyexchange.response.CustomResponse;
+import global.citytech.moneyexchange.platform.response.CustomResponse;
+import global.citytech.moneyexchange.platform.response.RestResponse;
+import global.citytech.moneyexchange.user.service.login.UserLoginRequest;
+import global.citytech.moneyexchange.user.service.login.UserLoginResponse;
 import global.citytech.moneyexchange.user.service.login.UserLoginService;
-import global.citytech.moneyexchange.user.dto.UsersDTO;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import jakarta.inject.Inject;
+
+import java.util.Optional;
 
 @Controller("/user")
 public class UserLoginController {
@@ -19,9 +24,9 @@ public class UserLoginController {
     }
 
     @Post("/login")
-    public CustomResponse loginUser(@Body UsersDTO usersDto){
-        userLoginService.loginUser(usersDto);
-        return new CustomResponse("Successfully Login",true);
+    public HttpResponse<RestResponse> loginUser(@Body UserLoginRequest request){
+         var response = userLoginService.loginUser(request);
+        return HttpResponse.ok(RestResponse.success(response.get()));
     }
 
 }
