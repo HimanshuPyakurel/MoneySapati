8598b4b4f94d53f14717acecbc98c5d2b95b34c0
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-11-16 10:11:52 +0545

    User list service implementation

--- src/main/java/global/citytech/moneyexchange/user/service/getall/UserGetAllServiceImpl.java
+++ src/main/java/global/citytech/moneyexchange/user/service/getall/UserGetAllServiceImpl.java
@@ -1,10 +1,13 @@
 package global.citytech.moneyexchange.user.service.getall;
 
-import global.citytech.moneyexchange.constraints.StatusAndRoleEnum;
-import global.citytech.moneyexchange.exception.CustomException;
-import global.citytech.moneyexchange.user.repository.Users;
+import global.citytech.moneyexchange.platform.constraints.StatusAndRoleEnum;
+import global.citytech.moneyexchange.platform.exception.CustomException;
+import global.citytech.moneyexchange.platform.security.ContextHolder;
 import global.citytech.moneyexchange.user.repository.UserRepository;
+import global.citytech.moneyexchange.user.repository.Users;
 import jakarta.inject.Inject;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,29 +21,67 @@ public class UserGetAllServiceImpl implements UserGetAllService {
     }
 
     @Override
-    public List<Users> getAllUsers(UserGetAllRequest request) {
-        Optional<Users> userDto = userRepository.findById(request.getUserId());
+    public List<UserGetAllResponse> getAllUsers() {
 
-        if (userDto.isPresent()) {
-            Users users = userDto.get();
+        System.out.println("......" + ContextHolder.get());
+        Optional<Users> userDto = userRepository.findById(ContextHolder.get().userID());
 
-            if ("Admin".equalsIgnoreCase(users.getUserRole().toString())) {
-                List<Users> allUsers = userRepository.findAll();
-                if (allUsers.isEmpty()) {
-                    throw new CustomException("No users found");
-                }
-                return allUsers;
+        if (userDto.get().getCheckStatus() == "PENDING")
+            throw new CustomException(400, "User Status not Verified");
+
+        if ("ADMIN".equalsIgnoreCase(ContextHolder.get().userRole())) {
+            List<UserGetAllResponse> responses = new ArrayList<>();
+            List<Users> allUsers = userRepository.findAll();
+
+            for (Users user : allUsers) {
+                UserGetAllResponse userResponse = convertToUserGetAllResponse(user);
+                responses.add(userResponse);
             }
-            else if(users.getCheckStatus().equals("Pending")){
-                throw new CustomException("User Status not Verified");
+            if (allUsers.isEmpty()) {
+                throw new CustomException(400, "No users found");
             }
-            else if ("Lender".equalsIgnoreCase(users.getUserRole().toString())) {
-                return userRepository.findByUserRole(StatusAndRoleEnum.BORROWER);
-            } else {
-                return userRepository.findByUserRole(StatusAndRoleEnum.LENDER);
-            }
+            return responses;
         }
-        throw new CustomException("User not found");
+
+        if ("LENDER".equalsIgnoreCase(ContextHolder.get().userRole())) {
+            List<UserGetAllResponse> responses = new ArrayList<>();
+            List<Users> borrower = userRepository.findByUserRole(StatusAndRoleEnum.BORROWER);
+
+            for (Users user : borrower) {
+                UserGetAllResponse userResponse = convertToUserGetAllResponse(user);
+                responses.add(userResponse);
+            }
+            return responses;
+
+        }
+        if ("BORROWER".equalsIgnoreCase(ContextHolder.get().userRole())) {
+            List<UserGetAllResponse> responses = new ArrayList<>();
+            List<Users> lender = userRepository.findByUserRole(StatusAndRoleEnum.LENDER);
+
+            for (Users user : lender) {
+                UserGetAllResponse userResponse = convertToUserGetAllResponse(user);
+                responses.add(userResponse);
+            }
+            return responses;
+        }
+        else throw new CustomException(400,"User not found");
     }
+
+    public UserGetAllResponse convertToUserGetAllResponse(Users user) {
+        UserGetAllResponse userResponse = new UserGetAllResponse();
+
+        userResponse.setId(user.getId());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setUserName(user.getUserName());
+        userResponse.setCitizenship(user.getCitizenship());
+        userResponse.setUserRoleEnum(user.getUserRole());
+        userResponse.setCheckStatus(user.getCheckStatus());
+        userResponse.setCheckBlacklist(user.getCheckBlacklist());
+        userResponse.setAvailableBalance(user.getAvailableBalance());
+
+        return userResponse;
+    }
+
 
 }
