2f1a3e7038f59815bf2db41fded794b582d42823
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-11-17 12:22:45 +0545

    user login service implementation

--- src/main/java/global/citytech/moneyexchange/user/service/login/UserLoginServiceImpl.java
+++ src/main/java/global/citytech/moneyexchange/user/service/login/UserLoginServiceImpl.java
@@ -1,52 +1,55 @@
 package global.citytech.moneyexchange.user.service.login;
 
-import global.citytech.moneyexchange.constraints.StatusAndRoleEnum;
-import global.citytech.moneyexchange.response.CustomResponse;
-import global.citytech.moneyexchange.exception.CustomException;
-import global.citytech.moneyexchange.transaction.repository.transaction.Transaction;
+import global.citytech.moneyexchange.platform.exception.CustomException;
+import global.citytech.moneyexchange.platform.security.SecurityUtils;
 import global.citytech.moneyexchange.transaction.repository.transaction.TransactionRepository;
 import global.citytech.moneyexchange.user.repository.Users;
-import global.citytech.moneyexchange.user.dto.UsersDTO;
 import global.citytech.moneyexchange.user.repository.UserRepository;
-import global.citytech.moneyexchange.utils.EmailUtils;
 import jakarta.inject.Inject;
-import jdk.jshell.Snippet;
 import org.springframework.util.DigestUtils;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public class UserLoginServiceImpl implements UserLoginService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
+    private final SecurityUtils securityUtils;
 
     @Inject
-    public UserLoginServiceImpl(UserRepository userRepository){
-        this.userRepository=userRepository;
+    public UserLoginServiceImpl(UserRepository userRepository, TransactionRepository transactionRepository,
+                                SecurityUtils securityUtils) {
+        this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
+        this.securityUtils = securityUtils;
     }
 
     @Override
-    public CustomResponse loginUser(UsersDTO usersDTO) {
-        usersDTO.setPassword(DigestUtils.md5DigestAsHex(usersDTO.getPassword().getBytes()));
-        Optional<Users> usersList = userRepository.findByEmailAndPassword(usersDTO.getEmail(), usersDTO.getPassword());
+    public Optional<UserLoginResponse> loginUser(UserLoginRequest request) {
+        request.setPassword(DigestUtils.md5DigestAsHex(request.getPassword().getBytes()));
+        Optional<Users> usersList = userRepository.findByEmailAndPassword(request.getEmail(), request.getPassword());
         if(usersList.isPresent()){
             String userStatus = usersList.get().getCheckStatus();
-            if(userStatus.equals("Verified")){
-//                this.sendEmail(UsersDTO userDto);
+            if(userStatus.equals("VERIFIED")){
 
-                return new CustomResponse(" User Logged in Successfully",true);
+                String token = securityUtils.token(usersList.get().getId(),usersList.get().getEmail(),
+                        usersList.get().getUserRole().toString());
+
+                return Optional.of(new UserLoginResponse(usersList.get().getUserName(),
+                        usersList.get().getUserRole().name(),"User Logged in Successfully",token));
+
             }else{
-                throw new CustomException("User Status not Verified");
+                throw new CustomException(400,"User Status Not Verified");
             }
         }else{
-            throw new CustomException("User not found. Check Username or password");
+            throw new CustomException(400,"User not found. Check Username or password");
         }
     }
 
-//    public void sendEmail(UsersDTO usersDTO){
+}
+
+
+//    public void sendEmail(UserLoginRequest request){
 //
 //    }
 
-
-}
